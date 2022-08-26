b13e374b6dff1d7755110fd9d4390faebbbb11d0
jimmyoops <jimmy85030@gmail.com>
2022-08-26 13:18:38 +0800

    typescript compile and interface practice

---
+++ server.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+interface todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => {
+    const todo = response.data as todo;
+
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+    console.log(`
+      The Todo with ID: ${id}
+      Has a title of ${title}
+      The current status is ${completed}
+    `);
+  })
